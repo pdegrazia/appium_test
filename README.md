9bf45a7972ff974733889432031c732b76e1275a
Paolo De Grazia <p.degrazia@gmail.com>
2016-08-08 20:57:03 +0100

    initial commit: basic structure to start ann appium test+dependencies

---
+++ README.md
@@ -0,0 +1,13 @@
+dependencies:
+
+appium server
+ Command line:
+  install node
+  install appium server
+  type appium --> server starts
+ 
+ Download from appium website
+
+Python:
+sudo pip install Appium-Python-Client
+
---
+++ from appium import webdriver.py
@@ -0,0 +1,13 @@
+from appium import webdriver
+import unittest
+
+# iOS environment
+desired_caps = {}
+desired_caps['platformName'] = 'iOS'
+desired_caps['platformVersion'] = '7.1'
+desired_caps['deviceName'] = 'iPhone Simulator'
+desired_caps['app'] = './Romeo_Feeder.app'
+
+self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+driver.launch_app()
