c4d60c7dcc729fca7d045a39d26ea5f6fdffe63c
Paolo De Grazia <p.degrazia@gmail.com>
2016-08-09 01:09:44 +0100

    introduced search for element + close app

--- mobile_first_test.py
+++ mobile_first_test.py
@@ -5,9 +5,15 @@ import unittest
 desired_caps = {}
 desired_caps['platformName'] = 'iOS'
 desired_caps['platformVersion'] = '9.3'
-desired_caps['deviceName'] = 'iPhone 5s'
-desired_caps['app'] = './Romeo_Feeder.app'
+desired_caps['deviceName'] = 'iPhone 6s Plus'
+desired_caps['app'] = './UICatalog.app'
 
-self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 driver.launch_app()
+
+el = driver.find_element_by_accessibility_id('Animation')
+
+assert(el is not None)
+
+driver.close_app()
